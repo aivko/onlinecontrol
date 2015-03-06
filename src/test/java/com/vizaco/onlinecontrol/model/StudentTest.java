5afc6c1e9b8f96ba600ca57971ac9970033b5e11
igor.vartanian <igor.vartanian@gmail.com>
2015-03-06 16:41:05 +0200

    Logger

---
+++ src/test/java/com/vizaco/onlinecontrol/model/StudentTest.java
@@ -0,0 +1,21 @@
+package com.vizaco.onlinecontrol.model;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.orm.hibernate4.SpringJtaSessionContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * Created by super on 3/6/15.
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"classpath:spring/application-context.xml"})
+public class StudentTest {
+
+    @Test
+    public void testGet(){
+
+    }
+
+}
