cebd15817f2ab832738fdfe059b5d17f434910d7
igor.vartanian <igor.vartanian@gmail.com>
2015-05-22 16:13:56 +0300

    e

---
+++ src/main/java/com/vizaco/onlinecontrol/model/json/ChangePassword.java
@@ -0,0 +1,53 @@
+package com.vizaco.onlinecontrol.model.json;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import javax.persistence.*;
+import java.util.Collection;
+import java.util.List;
+
+/**
+ * Simple business object representing a user.
+ *
+ */
+public class ChangePassword {
+
+    private String passwordOld;
+
+    private String password;
+
+    private String passwordConfirm;
+
+    public ChangePassword(String passwordOld, String password, String passwordConfirm) {
+        this.passwordOld = passwordOld;
+        this.password = password;
+        this.passwordConfirm = passwordConfirm;
+    }
+
+    public String getPasswordOld() {
+        return passwordOld;
+    }
+
+    public void setPasswordOld(String passwordOld) {
+        this.passwordOld = passwordOld;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPasswordConfirm() {
+        return passwordConfirm;
+    }
+
+    public void setPasswordConfirm(String passwordConfirm) {
+        this.passwordConfirm = passwordConfirm;
+    }
+}
