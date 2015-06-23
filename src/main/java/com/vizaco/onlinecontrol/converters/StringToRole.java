ddcb95aedb5c25af570f50f238d0a6fe25dff6d2
igor.vartanian <igor.vartanian@gmail.com>
2015-06-23 15:39:39 +0300

    java config

--- src/main/java/com/vizaco/onlinecontrol/converters/StringToRole.java
+++ src/main/java/com/vizaco/onlinecontrol/converters/StringToRole.java
@@ -1,31 +1,28 @@
-//package com.vizaco.onlinecontrol.converters;
-//
-//import com.vizaco.onlinecontrol.model.Role;
-//import com.vizaco.onlinecontrol.service.RoleService;
-//import com.vizaco.onlinecontrol.service.UserService;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.core.convert.converter.Converter;
-//import org.springframework.stereotype.Service;
-//
-///**
-// * Converter for Student control.
-// *
-// * Created: 01.02.2015
-// *
-// * @author Oleksandr Zamkovyi
-// * @since ???
-// */
-//public class StringToRole implements Converter<String, Role> {
-//
-//    private RoleService roleService;
-//
-//    @Autowired
-//    public StringToRole(RoleService roleService) {
-//        this.roleService = roleService;
-//    }
-//
-//    @Override
-//    public Role convert(String roleId) {
-//        return roleService.findRoleById(Long.parseLong(roleId));
-//    }
-//}
+package com.vizaco.onlinecontrol.converters;
+
+import com.vizaco.onlinecontrol.model.Role;
+import com.vizaco.onlinecontrol.service.RoleService;
+import com.vizaco.onlinecontrol.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.stereotype.Service;
+
+/**
+ * Converter for Student control.
+ *
+ * Created: 01.02.2015
+ *
+ * @author Oleksandr Zamkovyi
+ * @since ???
+ */
+@Service
+public class StringToRole implements Converter<String, Role> {
+
+    @Autowired
+    private RoleService roleService;
+
+    @Override
+    public Role convert(String roleId) {
+        return roleService.findRoleById(Long.parseLong(roleId));
+    }
+}
--- src/main/java/com/vizaco/onlinecontrol/converters/StringToStudent.java
+++ src/main/java/com/vizaco/onlinecontrol/converters/StringToStudent.java
@@ -1,30 +1,27 @@
-//package com.vizaco.onlinecontrol.converters;
-//
-//import com.vizaco.onlinecontrol.model.Student;
-//import com.vizaco.onlinecontrol.service.StudentService;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.core.convert.converter.Converter;
-//import org.springframework.stereotype.Service;
-//
-///**
-// * Converter for Student control.
-// *
-// * Created: 01.02.2015
-// *
-// * @author Oleksandr Zamkovyi
-// * @since ???
-// */
-//public class StringToStudent implements Converter<String, Student> {
-//
-//    private StudentService studentService;
-//
-//    @Autowired
-//    public StringToStudent(StudentService studentService) {
-//        this.studentService = studentService;
-//    }
-//
-//    @Override
-//    public Student convert(String studentId) {
-//        return studentService.findStudentById(Long.parseLong(studentId));
-//    }
-//}
+package com.vizaco.onlinecontrol.converters;
+
+import com.vizaco.onlinecontrol.model.Student;
+import com.vizaco.onlinecontrol.service.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.stereotype.Service;
+
+/**
+ * Converter for Student control.
+ *
+ * Created: 01.02.2015
+ *
+ * @author Oleksandr Zamkovyi
+ * @since ???
+ */
+@Service
+public class StringToStudent implements Converter<String, Student> {
+
+    @Autowired
+    private StudentService studentService;
+
+    @Override
+    public Student convert(String studentId) {
+        return studentService.findStudentById(Long.parseLong(studentId));
+    }
+}
