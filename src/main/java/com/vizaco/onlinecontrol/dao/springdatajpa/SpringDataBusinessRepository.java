759afe686af4a3fe1e07b943a40ecebe4b55df2b
igor.vartanian <igor.vartanian@gmail.com>
2015-08-07 16:56:31 +0300

    mysql

--- src/main/java/com/vizaco/onlinecontrol/dao/springdatajpa/SpringDataBusinessRepository.java
+++ src/main/java/com/vizaco/onlinecontrol/dao/springdatajpa/SpringDataBusinessRepository.java
@@ -3,15 +3,11 @@ package com.vizaco.onlinecontrol.dao.springdatajpa;
 import com.vizaco.onlinecontrol.dao.BusinessDao;
 import com.vizaco.onlinecontrol.model.Person;
 import com.vizaco.onlinecontrol.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.Repository;
 import org.springframework.data.repository.query.Param;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-import java.util.List;
-
-public interface SpringDataBusinessRepository extends BusinessDao, Repository<User, Integer> {
+public interface SpringDataBusinessRepository extends BusinessDao, JpaRepository<User, Integer> {
 
     @Override
     @Query("SELECT DISTINCT student FROM Student student where student.user =:user")
